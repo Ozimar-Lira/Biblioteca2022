55e43577e95f674433e51c52abc5e7daaab96bf4
Ozimar-Lira <ozimar.liraf@gmail.com>
2022-02-03 13:21:18 -0300

    Atividade 11 do Mantits do Projeto Biblioteca corrigida

--- Models/EmprestimoService.cs
+++ Models/EmprestimoService.cs
@@ -34,8 +34,47 @@ namespace Biblioteca.Models
         {
             using(BibliotecaContext bc = new BibliotecaContext())
             {
+              IQueryable<Emprestimo> query;
+
+              query = bc.Emprestimos.Include(e => e.Livro);
+               
+                if(filtro != null)
+                {
+                    //definindo dinamicamente a filtragem
+                    switch(filtro.TipoFiltro)
+                    {
+                        case "Usuario":
+
+                          query = bc.Emprestimos.Include(e => e.Livro).Where(e=> e.NomeUsuario.Contains(filtro.Filtro)); 
+                        
+                        break;
+
+                        case "Livro":
+
+                          List<Livro> LivrosFiltrados = bc.Livros.Where(l => l.Titulo.Contains(filtro.Filtro)).ToList();
+
+                          List<int> LivrosIds = new List<int>();
+                          for (int i = 0; i < LivrosFiltrados.Count; i ++)
+                          {
+                            LivrosIds.Add(LivrosFiltrados[i].Id);
+
+                          }
+
+                          query = bc.Emprestimos.Where(e => LivrosIds.Contains(e.LivroId));
+                
+                        break;
+
+                        default:
+                            query = bc.Emprestimos;
+                        break;
+                    }
+                }
+                else
+                {
+                    query = bc.Emprestimos.Include(e => e.Livro);
+                }
             
-              return bc.Emprestimos.Include(e => e.Livro).OrderBy(e => e.DataDevolucao).ToList();
+                return query.OrderBy(e => e.DataDevolucao).ToList();  
               
             }
         }
